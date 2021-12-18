e0936d328d63045040bfba2dc0a91c024f5e6d65
Slava971 <82147546+Slava971@users.noreply.github.com>
2021-12-18 17:59:25 +0300

    Add files via upload

---
+++ Slavik_weather_alpha/Slavik_weather_alpha/City_Name.swift
@@ -0,0 +1,27 @@
+//
+//  City_Name.swift
+//  Slavik_Weather
+//
+//  Created by user on 23.09.2021.
+//
+
+import UIKit
+
+class City_Name: UITableViewCell {
+
+    @IBOutlet weak var cityName: UILabel!
+    @IBOutlet weak var cityTemp: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
+
---
+++ Slavik_weather_alpha/Slavik_weather_alpha/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Slavik_weather_alpha
+//
+//  Created by Sofya Berlinskaya on 18.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+
+
